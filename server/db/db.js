461c5834d0ab0e1ccadf66f93ebc31bc877ff520
Timothy318 <41326306+Timothy318@users.noreply.github.com>
2021-11-02 08:12:06 -0700

    Updated db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,12 +1,7 @@
 const Sequelize = require("sequelize");
 
-// const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-//   logging: false
-// });
-
-const db = new Sequelize('messenger', 'postgres', '6184', {
-  host: 'localhost',
-  dialect: 'postgres'
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+  logging: false
 });
 
 module.exports = db;
